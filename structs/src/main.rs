895f33161f3fbff625f00acd395a2b53c36f53df
Samuel Lima <samuellima@Samuels-MacBook-Pro.local>
2022-01-01 15:02:29 -0300

    add methods examples

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,27 +1,51 @@
 fn main() {
-    let email = String::from("samuellfa3@gmail.com");
-    let username = String::from("sammurel");
-    let user = build_user(&email, &username);
-    println!("{}\n{}\n{}\n{}\n", user.email, user.username, user.active, user.sign_in_count);
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+    println!("{:?}", rect);
 }
 
-
-// Struct
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn build_user(email: &String, username: &String) -> User {
-    return User {
-        email: email.to_string(),
-        username: username.to_string(),
-        active: true,
-        sign_in_count: 1
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 }
+
+// fn main() {
+//     let email = String::from("samuellfa3@gmail.com");
+//     let username = String::from("sammurel");
+//     let user = build_user(&email, &username);
+//     println!("{}\n{}\n{}\n{}\n", user.email, user.username, user.active, user.sign_in_count);
+// }
+
+// Struct
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64
+// }
+
+// fn build_user(email: &String, username: &String) -> User {
+//     return User {
+//         email: email.to_string(),
+//         username: username.to_string(),
+//         active: true,
+//         sign_in_count: 1
+//     }
+// }
 
 // Tuple structs
 // fn main() {
